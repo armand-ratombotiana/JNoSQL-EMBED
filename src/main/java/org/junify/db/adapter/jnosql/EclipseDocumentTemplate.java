58b72a893a8dd53752bfc648246a65ac27f7a1d7
kael <judicael.ratombotiana@gmail.com>
2026-04-12 16:17:13 +0300

    fix: EclipseDocumentTemplate implements DocumentTemplate interface

--- src/main/java/org/junify/db/adapter/jnosql/EclipseDocumentTemplate.java
+++ src/main/java/org/junify/db/adapter/jnosql/EclipseDocumentTemplate.java
@@ -11,7 +11,7 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class EclipseDocumentTemplate {
+public class EclipseDocumentTemplate implements DocumentTemplate {
 
     private final DocumentCollection collection;
 
